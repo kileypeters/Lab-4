a2ea2063ef8ebac042f7db9ad81e15bbad7ce880
Kiley Peters <kiley.m.peters-1@ou.edu>
2019-02-07 11:11:09 -0600

    Added all files to repository

---
+++ Todo.java
@@ -0,0 +1,18 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-07
+OU ID_______________: 113371387
+OU EMAIL____________: kiley.m.peters-1@ou@ou.edu
+
+Tasks:
+1. XXX
+2. XXX
+
+Estimated Time (minutes):
+1. XXX
+2. XXx
+
+Actual Time (minutes):
+1. XXX
+2. XXX
+*/}
