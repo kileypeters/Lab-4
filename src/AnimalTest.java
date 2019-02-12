a25b0be481e517065e69ae0ac0b0b20a9017156e
Kiley Peters <kiley.m.peters-1@ou.edu>
2019-02-12 11:49:45 -0600

    Tested toString from Animal class

--- src/AnimalTest.java
+++ src/AnimalTest.java
@@ -1,3 +1,5 @@
+import org.junit.Assert;
+
 /**
  * Testing class for Product object
  *
@@ -23,5 +25,40 @@ public class AnimalTest
     }
 
     // TODO: test full constructor, getters, and toString
+    public void constructorTest() throws AssertException
+    {
+       // .getColor .getName .getHeight .getWeight
+    	//String color, String name, double weight, double height
+    	
+    	String color1 = "blue";
+    	String name1 = "name";
+    	double weight1 = 0.0;
+    	double height1 = 0.0;
+    	Animal animal1 = new Animal(color1, name1, weight1, height1);
+    	
+    	String color2 = "blue";
+    	String name2 = "name";
+    	double weight2 = 0.0;
+    	double height2 = 0.0;
+    	Animal animal2 = new Animal(color2, name2, weight2, height2);
+    	
+    	Assert.assertEquals(animal1, animal1);
+    	Assert.assertEquals(animal2, animal2);
+    	
+    }
+    public void gettersTest()
+    {
+    	
+    }
+    public void toStringTest()
+    {
+    	String color1 = "blue";
+    	String name1 = "name";
+    	double weight1 = 0.0;
+    	double height1 = 0.0;
+    	Animal animal1 = new Animal(color1, name1, weight1, height1);
+    	
+    	Assert.assertEquals("name, a blue-colored animal. 0.0 pounds, 0.0 inches\n", animal1.toString());
+    }
 }
 
